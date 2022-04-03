9f61062f0b51cee3b673e4b64dc5256bbffdc090
codeComet <unknownmind10@gmail.com>
2022-04-03 23:31:19 +0600

    navbar completed

---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Box, Typography } from "@mui/material";
+import Logo from "../../assets/img/logo.png";
+import "./styles.css";
+
+export default function Nav() {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        padding: "20px 0px",
+      }}
+    >
+      <Box sx={{ display: "flex", alignItems: "center", flexGrow: 1 }}>
+        <img src={Logo} alt="logo" style={{ marginRight: "10px" }} />
+        <Typography className="logoText">Space</Typography>
+      </Box>
+
+      <Box sx={{ flexGrow: 2 }}></Box>
+
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          flexGrow: 1,
+          justifyContent: "space-between",
+        }}
+      >
+        <Typography className="navText" style={{ color: "#8C21ED" }}>
+          Home
+        </Typography>
+        <Typography className="navText">About us</Typography>
+        <Typography className="navText">Story</Typography>
+        <Typography className="navText">Blog</Typography>
+        <button className="navBtn">Contact us</button>
+      </Box>
+    </Box>
+  );
+}
